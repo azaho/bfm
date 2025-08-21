6e06e0faca45b999ec5ce4981625c46aed295892
zaho <zaho@orcd-login003.mit.edu>
2025-08-21 09:41:59 -0400

    added flexibility for force remove idx

--- training_setup/training_setup.py
+++ training_setup/training_setup.py
@@ -93,17 +93,23 @@ class TrainingSetup:
         batch['data'] = batch['data'] / (torch.std(batch['data'], dim=[0, 2], keepdim=True) + 1)
         return batch
     
-    def _preprocess_subset_electrodes(self, batch, output_selected_idx=False, keys=['data'], allowed_remove_idx=None):
+    def _preprocess_subset_electrodes(self, batch, output_selected_idx=False, keys=['data'], allowed_remove_idx=None, force_remove_idx=None):
         # Find minimum number of electrodes in batch
         batch_size = batch[keys[0]].shape[0]
         n_electrodes = batch[keys[0]].shape[1]
-        subset_n_electrodes = min(n_electrodes, self.config['training']['max_n_electrodes']) if self.config['training']['max_n_electrodes']>0 else n_electrodes
-        remove_n_electrodes = n_electrodes - subset_n_electrodes
 
         if allowed_remove_idx is None:
-            allowed_remove_idx = torch.arange(n_electrodes)
-        selected_remove_idx = allowed_remove_idx[torch.randperm(len(allowed_remove_idx))[:remove_n_electrodes]]
-        selected_idx = torch.arange(n_electrodes)
+            allowed_remove_idx = torch.arange(n_electrodes, device=batch[keys[0]].device, dtype=torch.long)
+        if force_remove_idx is None:
+            force_remove_idx = torch.tensor([], device=batch[keys[0]].device, dtype=torch.long)
+        elif not isinstance(force_remove_idx, torch.Tensor):
+            force_remove_idx = torch.tensor(force_remove_idx, device=batch[keys[0]].device, dtype=torch.long)
+
+        subset_n_electrodes = min(n_electrodes, self.config['training']['max_n_electrodes']) if self.config['training']['max_n_electrodes']>0 else n_electrodes
+        remove_n_electrodes = max(0, n_electrodes - subset_n_electrodes - len(force_remove_idx))
+
+        selected_remove_idx = torch.cat([allowed_remove_idx[torch.randperm(len(allowed_remove_idx))[:remove_n_electrodes]], force_remove_idx])
+        selected_idx = torch.arange(n_electrodes, device=batch[keys[0]].device, dtype=torch.long)
         selected_idx = selected_idx[~torch.isin(selected_idx, selected_remove_idx)]
 
         if 'electrode_labels' in batch:
