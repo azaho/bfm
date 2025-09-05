83bac4eeec80b26dc0336bf98e74508c6f7a6c15
Andrii Zahorodnii <zaho@OPENMIND7.mit.edu>
2025-09-05 14:13:57 -0400

    complete evaluation cleanup

--- bfm/pretrain.py
+++ bfm/pretrain.py
@@ -17,7 +17,6 @@ load_dotenv() # Load environment variables from .env file
 
 from bfm.training.optimizer.builders import build_optimizers, build_schedulers
 from bfm.subject.datasets.dataset import load_subjects
-from bfm.evaluation.neuroprobe_tasks import FrozenModelEvaluation_SS_SM
 from bfm.training.setup_registry import setups
 from bfm.core.logger import log
 from bfm.training.training_config import (
@@ -86,64 +85,20 @@ model_params = training_setup.model_parameters(verbose=True)
 optimizers = build_optimizers(model_params, config)
 schedulers = build_schedulers(optimizers, config, training_setup)
 
-# Below for all the tasks in Neuroprobe
-# eval_tasks = ['frame_brightness', 'global_flow', 'local_flow', 'global_flow_angle', 'local_flow_angle', 'face_num', 'volume', 'pitch', 'delta_volume', 
-#               'delta_pitch', 'speech', 'onset', 'gpt2_surprisal', 'word_length', 'word_gap', 'word_index', 'word_head_pos', 'word_part_speech', 'speaker']
-eval_tasks = config['training']['eval_tasks'].split(',')
-# Filter out empty strings
-eval_tasks = [task for task in eval_tasks if task.strip()]
-
-if eval_tasks:  # Only create evaluation if there are tasks
-    evaluation = FrozenModelEvaluation_SS_SM(
-        # model evaluation function
-        model_preprocess_functions=training_setup.get_preprocess_functions(pretraining=False),
-        model_evaluation_function=training_setup.generate_frozen_features,
-        eval_aggregation_method=config['cluster']['eval_aggregation_method'],
-        # benchmark parameters 
-        eval_names=eval_tasks, lite=True,
-        subject_trials=[(all_subjects[subject_identifier], trial_id) for subject_identifier, trial_id in config['training']['eval_subject_trials']],
-        # dataloader parameters
-        device=device,
-        dtype=config['training']['data_dtype'],
-        batch_size=config['training']['batch_size'],
-        num_workers_eval=config['cluster']['num_workers_eval'],
-        prefetch_factor=config['cluster']['prefetch_factor'],
-    )
-else:
-    evaluation = None
-
 ### WANDB SETUP ###
 
 if wandb: 
     wandb_dir = f"{RUNS_DIR}/wandb"
     os.makedirs(wandb_dir, exist_ok=True)
-    if len(config['cluster']['wandb_entity']) > 0:
-        wandb.init(project=config['cluster']['wandb_project'], name=config['cluster']['wandb_name'], id=config['cluster']['wandb_name'],
-                    entity=config['cluster']['wandb_entity'],
-                    config=config, settings=wandb.Settings(init_timeout=1000), dir=wandb_dir)
-    else:
-        wandb.init(project=config['cluster']['wandb_project'], name=config['cluster']['wandb_name'], id=config['cluster']['wandb_name'],
-                    config=config, settings=wandb.Settings(init_timeout=1000), dir=wandb_dir)
-
-### EVALUATION OF THE MODEL BEFORE TRAINING ###
-
-eval_results = {}
-if config['cluster']['eval_at_beginning'] and evaluation is not None:
-    log(f"Evaluating model...", priority=0)
-    training_setup.eval_mode()
-    # 
-    eval_raw = evaluation.evaluate_on_all_metrics(quick_eval=config['cluster']['quick_eval'], only_keys_containing='auroc/average', raw_data=True, key_prefix="raw_")
-    eval_results.update(eval_raw)
-    log(f"eval_raw: {eval_raw}", priority=0)
-    #
-    eval_full_model = evaluation.evaluate_on_all_metrics(quick_eval=config['cluster']['quick_eval'], only_keys_containing='auroc/average')
-    log(f"eval_full_model: {eval_full_model}", priority=0)
-    eval_results.update(eval_full_model)
-    #
-    if wandb: wandb.log(eval_results, step=1)
-    del eval_full_model, eval_raw
-    torch.cuda.empty_cache()
-    gc.collect()
+    wandb.init(
+        project=config['cluster']['wandb_project'], 
+        name=config['cluster']['wandb_name'], 
+        id=config['cluster']['wandb_name'],
+        entity=config['cluster']['wandb_entity'] if len(config['cluster']['wandb_entity']) > 0 else None,
+        config=config, 
+        settings=wandb.Settings(init_timeout=1000), 
+        dir=wandb_dir
+    )
 
 ### TRAINING ###
 
@@ -221,13 +176,6 @@ for epoch_i in range(config['training']['n_epochs']):
         losses_string = f" / ".join([f"{k.split('_')[1]}: {v:.4f}" for k, v in test_loss_dict.items() if 'accuracy' not in k])
         accuracy_string = f" / ".join([f"{k.split('_')[1]}: {v:.4f}" for k, v in test_loss_dict.items() if 'accuracy' in k])
         log(f"Test loss: {eval_results['test_loss']:.4f} ({losses_string}), Accuracies: {accuracy_string}", priority=0)
-        if (epoch_i+1) % config['cluster']['eval_model_every_n_epochs'] == 0 and evaluation is not None:
-            evaluation_results_strings = evaluation.evaluate_on_all_metrics(quick_eval=config['cluster']['quick_eval'], only_keys_containing='auroc/average')
-            eval_results.update(evaluation_results_strings)
-            log("eval_full_model" + str(evaluation_results_strings))
-            del evaluation_results_strings
-            torch.cuda.empty_cache()
-            gc.collect()
         time_remaining = (time.time() - epoch_start_time) * (config['training']['n_epochs'] - (epoch_i + 1))
         days = int(time_remaining // (24 * 3600))
         log(f"Epoch {epoch_i+1}/{config['training']['n_epochs']}, Estimated time remaining: {days}d, {time.strftime('%H:%M:%S', time.gmtime(time_remaining % (24 * 3600)))}", priority=0)
